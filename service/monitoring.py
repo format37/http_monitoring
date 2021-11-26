6f4f328b02c649adab3575fba991ec79b56c9a6e
alex <format37@gmail.com>
2021-11-26 11:44:59 +0300

    modified:   service/monitoring.py

--- service/monitoring.py
+++ service/monitoring.py
@@ -10,13 +10,25 @@ import re
 def main():
     name = os.environ.get('SERVICE_NAME', 'noname')
     url = os.environ.get('SERVICE_URL', '')
+    regular_sleep = os.environ.get('REGULAR_SLEEP', '1')
+    error_sleep = os.environ.get('ERROR_SLEEP', '60')
     chat_id = os.environ.get('TELEGRAM_CHAT', '')
     api_token = os.environ.get('TELEGRAM_BOT_TOKEN', '')
     bot = telebot.TeleBot(api_token, parse_mode=None)
-    
+    last_try_succesfull = True
+
     while True:
         r = requests.get(url)
-        if r.status_code != 200:
+        if r.status_code == 200:
+            if not last_try_succesfull:
+                bot.send_message(
+                    chat_id, 
+                    name+'\n'+str(r.status_code)+': OK\n'+url+'\n'+dt
+                )
+            last_try_succesfull = True
+            sleep(regular_sleep)
+        else:
+            last_try_succesfull = False
             dt = str(datetime.now())
             try:
                 r.encoding = 'utf-8'
@@ -31,8 +43,7 @@ def main():
                 chat_id, 
                 name+'\n'+message+'\n'+url+'\n'+dt
                 )
-            sleep(59)
-        sleep(1)
+            sleep(error_sleep)
 
 
 if __name__ == '__main__':
